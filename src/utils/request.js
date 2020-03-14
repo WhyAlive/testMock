c89da80d66c8fb0780a8c99993e43294839edb44
SuPeng <974204136@qq.com>
2020-03-14 16:02:48 +0800

    初始化项目

---
+++ src/utils/request.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+// axios.defaults.baseURL = 'http://localhost:8080/'
+const instance = axios.create({
+  baseURL: 'http://localhost:8080/'
+})
+export default (url, method, data) => {
+  return instance({
+    url,
+    method,
+    [method.toLowerCase() === 'get' ? 'params' : 'data']: data
+  })
+}
